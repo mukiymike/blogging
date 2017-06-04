321c6c9e9cc61b982c54415067f45594d14d80f0
Lemmah <lemayiannakolah@gmail.com>
2017-06-04 03:59:35 +0300

    Include files for the admin section.

---
+++ admin/includes/segments/metaEnLinks.php
@@ -0,0 +1,9 @@
+<!-- The Meta and LInks -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width" />
+<meta name="description" content="Take a look at all the Hot and trending stuff going around Multimedia University of Kenya.This is the number one blogspot featuring the craziest campus experience"/>
+<meta name="keywords" content="Multimedia University of kenya, kenyan campus, divas, mmu""/>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
---
+++ admin/includes/segments/navigationBar.php
@@ -0,0 +1,16 @@
+ <!-- TOP NAV WITH LOGO -->
+<header class="margin-bottom">
+   <nav class="navbar navbar-default">
+     <div class="container-fluid">
+       <div class="navbar-header">
+         <a class="navbar-brand" href="../">MMU Shadows - Admin.</a>
+       </div>
+       <ul class="nav navbar-nav">
+         <li class="active"><a href="#">Blog</a></li>
+         <li><a href="#">Users</a></li>
+         <li><a href="#">View Website</a></li>
+         <li><a href="#">Logout</a></li>
+       </ul>
+     </div>
+   </nav>
+</header>
