ac519c981c3202336e005cae2c24a90241bed49d
Lemmah <lemayiannakolah@gmail.com>
2017-06-04 13:26:26 +0300

    Purely using bootstrap in admin section.

--- admin/includes/segments/navigationBar.php
+++ admin/includes/segments/navigationBar.php
@@ -6,10 +6,23 @@
          <a class="navbar-brand" href="../">MMU Shadows - Admin.</a>
        </div>
        <ul class="nav navbar-nav">
-         <li class="active"><a href="#">Blog</a></li>
-         <li><a href="#">Users</a></li>
-         <li><a href="#">View Website</a></li>
-         <li><a href="#">Logout</a></li>
+         <!-- Get the state of the active class -->
+         <li <?php
+                if ($active=="blog") {
+                  echo "class='active'";
+                } else {
+                  echo " ";
+                }
+               ?>><a href="index.php">Blog</a></li>
+         <li <?php
+                if ($active=="users") {
+                  echo "class='active'";
+                } else {
+                  echo " ";
+                }
+               ?>><a href="users.php">Users</a></li>
+         <li ><a href="../">View Website</a></li>
+         <li ><a href="logout.php">Logout&nbsp;&nbsp;<span class="glyphicon glyphicon-log-in"></span></a></li>
        </ul>
      </div>
    </nav>
