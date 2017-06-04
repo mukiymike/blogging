2671da312d0417e129c38557bce90042dde4b1ac
Lemmah <lemayiannakolah@gmail.com>
2017-06-04 04:00:29 +0300

    Renamed index.html to index.php
    This is for safety while executing php statements.

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+   <title> Multimedia Shadows </title>
+
+   <?php
+   include "includes/segments/metaEnLinks.php";
+   ?>
+
+   <!-- PageSpeed and Google Ads. -->
+   <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+   <script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+       google_ad_client: "ca-pub-5385801574160018",
+       enable_page_level_ads: true
+     });
+   </script>
+
+   </head>
+   <body class="size-1140">
+
+   <!-- Page Views by google analytics -->
+   <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+     ga('create', 'UA-98708265-1', 'auto');
+     ga('send', 'pageview');
+   </script>
+
+     <?php
+      include "includes/segments/navigationBar.php";
+      ?>
+
+      <!-- MAIN SECTION -->
+      <section id="home-section" class="line">
+         <div class="margin">
+            <!-- ARTICLES -->
+            <div class="s-12 l-9">
+                           <!-- ARTICLE 1 -->
+               <article class="post-1 line">
+                  <!-- image -->
+                  <div class="s-12 l-6 post-image">
+                     <img src="img/IMG-20170511-WA0002.jpg" alt="Fashion 1">
+                     </a>
+
+                  </div>
+                  <!-- text -->
+                  <div class="s-12 l-5 post-text">
+                     <a href="post-1.html">
+                     </a>
+                     <H2>Multimedia University</H2>
+                     <p>Multimedia Shadows is the first and only Multimedia University entertainment blog.This is the realest site that posts on what is hot and trending in MMU Kenya.</p>
+
+                  </div>
+
+               <!-- ARTICLE 2-->
+               <article class="post-1 line">
+                  <!-- image -->
+
+               <article class="post-2 right-align line">
+                  <div class="s-12 l-6 post-image">
+                     <a href="post-1.html">
+                     <img src="img/IMG-20170519-WA0057.jpg" alt="Fashion 1">
+                     </a>
+
+                  </div>
+                  <!-- text -->
+                  <div class="s-12 l-5 post-text">
+                     <a href="post-1.html">
+                        <h2>Multimedia Top Ten Finest Divas</h2>
+                     </a>
+                     <p> With its high number of diverse courses Multimedia University of Kenya has many Pretty divas who not only have the most beautiful faces but also the cut edging curves.... </p>
+                     <a class="continue-reading" href="post-1.html">Continue reading</a>
+
+                  </div>
+                  <!-- date -->
+                 <!-- date -->
+                  <div class="s-12 l-1 post-date">
+                     <p class="date">03</p>
+                     <p class="month">June</p>
+                  </div>
+               </article>
+
+               <!-- ARTICLE 2 -->
+               <article class="post-2 left-align line">
+                  <!-- image -->
+                  <div class="s-12 l-6 post-image">
+                     <a href="post-2.html">
+                     <img src="img/post2.jpg" alt="fashion2.jpg">
+                     </a>
+                  </div>
+                  <!-- text -->
+                  <div class="s-12 l-5 post-text">
+                     <a href="post-2.html">
+                        <h2>Trend Setters</h2>
+                     </a>
+                     <p>These gentlemen and ladies define the elegance of dressing and they bring out the new sence of fashion in the vicinity of the campus.  </p>
+                     <a class="continue-reading" href="post-2.html">Continue reading</a>
+
+                  </div>
+                  <!-- date -->
+                  <!-- date -->
+                  <div class="s-12 l-1 post-date">
+                     <p class="date">03</p>
+                     <p class="month">June</p>
+                  </div>
+
+               </article>
+
+               <!-- ARTICLE 3 -->
+               <article class="post-3 line">
+                  <!-- image -->
+
+               <article class="post-3 right-align line">
+                  <div class="s-12 l-6 post-image">
+                     <a href="post-3.html">
+                     <img src="img/DonCliq_Photography Fam 20170519_151807.jpg" alt="Fashion 3">
+                     </a>
+                  </div>
+                  <!-- text -->
+                  <div class="s-12 l-5 post-text">
+                     <a href="post-3.html">
+                        <h2>Finest Photography</h2>
+                     </a>
+                     <p>Ever found a hard time looking for the perfect photographers?MMU Shadows gives you a top list of the three finest photographers to choose from.</p>
+                               <a class="continue-reading" href="post-3.html">Continue reading</a>
+                  </div>
+                  <!-- date -->
+                  <div class="s-12 l-1 post-date">
+                     <p class="date">03</p>
+                     <p class="month">June</p>
+                  </div>
+               </article>
+
+               <!-- ARTICLE 4 -->
+               <article class="post-4 left-align line">
+                  <!-- image -->
+                  <div class="s-12 l-6 post-image">
+                     <a href="post-4.html">
+                     <img src="img/mercy wekesa 20170519_151743.jpg" alt="Fashion 4">
+                     </a>
+                  </div>
+                  <!-- text -->
+                  <div class="s-12 l-5 post-text">
+                     <a href="post-4.html">
+                        <h2>Multimedia Forbes List</h2>
+                     </a>
+                     <p>Ever thought of the richest kids in the campus, shadows gives you the insight of the students who source out their cash from different sources.</p>
+                     <a class="continue-reading" href="post-4.html">Continue reading</a>
+                  </div>
+                  <article class="post-3 right-align line">
+                  <div class="s-12 l-6 post-image">
+                     <a href="post-5.html">
+                     <img src="img/IMG-20170519-WA045.jpg" alt="Fashion 3">
+                     </a>
+                  </div>
+                  <!-- text -->
+                  <div class="s-12 l-5 post-text">
+                     <a href="post-3.html">
+                        <h2>Finest Hunks</h2>
+                     </a>
+                     <p>Ladies we bring you the hottest gentlemen in the campus.</p>
+                               <a class="continue-reading" href="post-5.html">Continue reading</a>
+                  </div>
+                  <!-- date -->
+                  <div class="s-12 l-1 post-date">
+                     <p class="date">03</p>
+                     <p class="month">June</p>
+                  </div>
+               </article>
+
+               </article>
+
+               <!-- ARTICLE 5 -->
+               <article class="post-5 line">
+                  <!-- text -->
+                  <div class="s-12 l-11 post-text">
+                     <a href="comments.html">
+                        <h2>Comments</h2>
+                     </a>
+                     <p>Your comments and views will be highly appreciated</p>
+                     <a class="continue-reading" href="comments.html">Comment</a>
+                  </div>
+                  <!-- date -->
+                  <div class="s-12 l-1 post-date">
+                     <p class="date">03</p>
+                     <p class="month">June</p>
+                  </div>
+
+               </article>
+            </div>
+
+
+            <!-- SIDEBAR -->
+            <div class="s-12 l-3">
+               <aside>
+
+                  <!-- NEWS 1 -->
+                  <img src="img/Screenshot_20170519-004151.png" alt="fashion">
+                  <div class="aside-block margin-bottom">
+                     <h3>Entertainment Blog</h3>
+                     <p>This is the realest site that posts on what is hot and trending in MMU Kenya.This blog gives you the sneack sniff on the top ten prettiest chicks and top five hot dudes in Multimedia.Being the number one media and technology university,this blog gives you a diverse events line up and the best places to hang out.</p>
+                  </div>
+                  <!-- NEWS 2 -->
+                  <img src="img/v1.dDsyMDE4MzQ7ajsxNzMyNzsxMjAwOzE3NzI7MjY1OA" alt="News 2">
+                  <div class="aside-block margin-bottom">
+                     <h3>Scandal Slot!!</h3>
+                     <p>This is a special segment that gives you all the udaku and the mpashoz. Stay tuned for the heartbreaks or any guy caught cheating behind the blocks or the streets of Rongai.We dont spare looses chilez either. </p>
+                  </div>
+
+                  <!-- AD REGION -->
+                  <div class="advertising margin-bottom">
+                     <img src="img/IMG-20170531-WA0006.jpg" alt="ad banner">
+                  </div>
+               </aside>
+            </div>
+         </div>
+      </section>
+
+      <?php
+      include "includes/segments/footerSect.php";
+      ?>
+
+   </body>
+</html>
