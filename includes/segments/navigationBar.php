a4a8305c1a657dd7bad1faf57311cfd20eef4f33
Lemmah <lemayiannakolah@gmail.com>
2017-06-04 03:59:11 +0300

    Working include files.
    I have just made the navigation bar, footer, links and meta data include files since they are repetitive across all the pages.

---
+++ includes/segments/navigationBar.php
@@ -0,0 +1,37 @@
+ <!-- TOP NAV WITH LOGO -->
+      <header class="margin-bottom">
+         <div class="line">
+            <nav>
+               <div class="top-nav">
+                  <p class="nav-text"></p>
+                  <a class="logo" href="index.html">
+                  Multimedia<span>Shadows</span>
+                  </a>
+                  <h1>Multimedia Sensation</h1>
+                  <ul class="top-ul right">
+                     <li>
+                        <a href="index.html">Blog</a>
+                     </li>
+                     <li>
+                        <a href="about.html">Trending</a>
+                     </li>
+                      <li>
+                        <a href="event.html">Events</a>
+                     </li>
+                     <li>
+                        <a href=".php">Login</a>
+                     </li>
+
+                      <div class="social right">
+                        <a target="_blank" href="https://www.facebook.com/mmushadows">
+                        <i class="icon-facebook_circle icon2x"></i>
+                        </a>
+                        <a target="_blank" href="https://twitter.com/mmushadows">
+                        <i class="icon-twitter_circle icon2x"></i>
+                        </a>
+                     </div>
+                  </ul>
+               </div>
+            </nav>
+         </div>
+      </header>
