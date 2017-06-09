a2c856fa6682f035c8ad1427378b356e35f2945e
Lemmah <lemayiannakolah@gmail.com>
2017-06-09 13:00:40 +0300

    Text editor line endings.

--- classes/class.user.php
+++ classes/class.user.php
@@ -5,26 +5,26 @@ include('class.password.php');
 class User extends Password{
 
     private $db;
-	
+
 	function __construct($db){
 		parent::__construct();
-	
+
 		$this->_db = $db;
 	}
 
 	public function is_logged_in(){
 		if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
 			return true;
-		}		
+		}
 	}
 
-	private function get_user_hash($username){	
+	private function get_user_hash($username){
 
 		try {
 
 			$stmt = $this->_db->prepare('SELECT password FROM blog_members WHERE username = :username');
 			$stmt->execute(array('username' => $username));
-			
+
 			$row = $stmt->fetch();
 			return $row['password'];
 
@@ -33,23 +33,23 @@ class User extends Password{
 		}
 	}
 
-	
-	public function login($username,$password){	
+
+	public function login($username,$password){
 
 		$hashed = $this->get_user_hash($username);
-		
+
 		if($this->password_verify($password,$hashed) == 1){
-		    
+
 		    $_SESSION['loggedin'] = true;
 		    return true;
-		}		
+		}
 	}
-	
-		
+
+
 	public function logout(){
 		session_destroy();
 	}
-	
+
 }
 
 
