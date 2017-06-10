17005fff2e851fb7325704cd9df2c74dbe160d0d
Lemmah <lemayiannakolah@gmail.com>
2017-06-10 11:44:31 +0300

    Error handling in uploading image.

--- admin/uploadImg.php
+++ admin/uploadImg.php
@@ -10,35 +10,41 @@ if(isset($_POST["submit"])) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        $error[] = "File is not an image.";
         $uploadOk = 0;
     }
 }
-// Check if file already exists
+
+// Check if image already exists and replace it for editing
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    unlink($target_file);
+    echo 'File '.$target_file.' has been deleted';
+    $uploadOk = 1;
+  }else{
+    $error[] = "There was an error while trying to update the image. Please try again or call Lemayian, the admin.";
     $uploadOk = 0;
-}
+  }
+
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 5000000) {
-    echo "Sorry, your file is too large.";
+    $error[] = "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $error[] = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    $error[] = "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        $error[] = "Sorry, there was an error uploading your file. Please try again.";
     }
 }
 ?>
