b4f3d380518bdb0be0ea1a2dfe0676b5ed87be90
Lemmah <lemayiannakolah@gmail.com>
2017-06-18 21:41:57 +0300

    The connection and the db config.

---
+++ admin/includes/connect.php
@@ -0,0 +1,44 @@
+<?php
+ob_start();
+session_start();
+
+//database credentials
+define('DBHOST','localhost');
+define('DBUSER','yourusername');
+define('DBPASS','yourpassword');
+define('DBNAME','php_blogging');
+
+$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//set timezone
+date_default_timezone_set('Europe/London');
+
+//load classes as needed
+function __autoload($class) {
+
+   $class = strtolower($class);
+
+	//if call from within assets adjust the path
+   $classpath = 'classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+
+	//if call from within admin adjust the path
+   $classpath = '../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+
+	//if call from within admin adjust the path
+   $classpath = '../../classes/class.'.$class . '.php';
+   if ( file_exists($classpath)) {
+      require_once $classpath;
+	}
+
+}
+
+$user = new User($db);
+?>
